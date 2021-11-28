8b6a32a13f22cbadd3f0959bc8e18b70abf20ee8
keisuke <mrtambourineman8358@gmail.com>
2021-11-29 01:15:25 +0900

    on the way

---
+++ docker-entrypoint-initdb.d/00_create_tables.sql
@@ -0,0 +1,154 @@
+
+-- ユーザーテーブル --
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) DEFAULT NOT NULL,
+  `email` VARCHAR(255) DEFAULT NOT NULL,
+  `password` VARCHAR(255) DEFAULT NOT NULL,
+  `nickname` VARCHAR(255) DEFAULT NOT NULL,
+  `first_name` VARCHAR(255) DEFAULT NOT NULL,
+  `last_name` VARCHAR(255) DEFAULT NOT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `group_id` tinyint(4) DEFAULT 1,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ルアーテーブル --
+
+DROP TABLE IF EXISTS lures;
+
+CREATE TABLE lures (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) DEFAULT NOT NULL,
+  `lure_type_id` tinyint(4) DEFAULT NULL,
+  `user_id` int(255) DEFAULT NULL,
+  `color_id` int(255) DEFAULT NULL,
+  `size_id` int(255) DEFAULT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ルアータイプテーブル --
+
+DROP TABLE IF EXISTS lure_types;
+
+CREATE TABLE lure_types (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `type_name` VARCHAR(255) DEFAULT NOT NULL,
+  `company_name` VARCHAR(255) DEFAULT NOT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ヒットパターンテーブル --
+
+DROP TABLE IF EXISTS hit_patterns;
+
+CREATE TABLE hit_patterns (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) DEFAULT NULL,
+  `lure_id` int(255) DEFAULT NULL,
+  `tackle_id` int(255) DEFAULT NULL,
+  `speed` int(255) DEFAULT NULL,
+  `depth` VARCHAR(255) DEFAULT NULL,
+  `weather` VARCHAR(255) DEFAULT NULL,
+  `result` VARCHAR(255) DEFAULT NULL,
+  `field_id` int(255) DEFAULT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- タックルテーブル --
+
+DROP TABLE IF EXISTS tackles;
+
+CREATE TABLE tackles (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) DEFAULT NULL,
+  `rod_id` int(255) DEFAULT NULL,
+  `reel_id` int(255) DEFAULT NULL,
+  `line_id` int(255) DEFAULT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ロッドテーブル --
+
+DROP TABLE IF EXISTS rods;
+
+CREATE TABLE rods (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) DEFAULT NULL,
+  `name` VARCHAR(255) DEFAULT NULL,
+  `hardness` tinyint(4) DEFAULT 0,
+  `length` tinyint(4) DEFAULT 0,
+  `name`  VARCHAR(255) DEFAULT NULL,
+  `company_name`  VARCHAR(255) DEFAULT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- リールテーブル --
+
+DROP TABLE IF EXISTS reels;
+
+CREATE TABLE reels (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) DEFAULT NULL,
+  `name` VARCHAR(255) DEFAULT NULL,
+  `type_number` int(255) DEFAULT NULL,
+  `company_name`  VARCHAR(255) DEFAULT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ロッドテーブル --
+
+DROP TABLE IF EXISTS rods;
+
+CREATE TABLE rods (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) DEFAULT NULL,
+  `name` VARCHAR(255) DEFAULT NULL,
+  `hardness` tinyint(4) DEFAULT 0,
+  `length` tinyint(4) DEFAULT 0,
+  `company_name`  VARCHAR(255) DEFAULT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ラインテーブル --
+
+DROP TABLE IF EXISTS lines;
+
+CREATE TABLE lines (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) DEFAULT NULL,
+  `name` VARCHAR(255) DEFAULT NULL,
+  `line_type` tinyint(4) DEFAULT 0,
+  `thickness` tinyint(4) DEFAULT 0,
+  `company_name`  VARCHAR(255) DEFAULT NULL,
+  `delete_flg` tinyint(4) DEFAULT 0,
+  `created_at` Datetime DEFAULT NULL,
+  `updated_at` Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
---
+++ docker-entrypoint-initdb.d/01_insert_data.sql
@@ -0,0 +1,6 @@
+
+-- ユーザー --
+-- INSERT INTO users (id, name, email) VALUES (1, "Yamada", "yamada@example.com");
+-- INSERT INTO users (id, name, email) VALUES (2, "Tanaka", "tanaka@example.com");
+
+
