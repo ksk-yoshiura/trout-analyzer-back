e0fd8c97927930063b29da52dfd72d20652fc01a
keisuke <mrtambourineman8358@gmail.com>
2021-12-11 21:57:36 +0900

    unnecessary

--- docker-entrypoint-initdb.d/01_insert_data.sql
+++
@@ -1,6 +0,0 @@
-
--- ユーザー --
--- INSERT INTO users (id, name, email) VALUES (1, "Yamada", "yamada@example.com");
--- INSERT INTO users (id, name, email) VALUES (2, "Tanaka", "tanaka@example.com");
-
-
