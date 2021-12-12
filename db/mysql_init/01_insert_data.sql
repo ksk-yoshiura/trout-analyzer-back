9557c8d05e7134951e76a9388c0e72abf7754fcf
keisuke <mrtambourineman8358@gmail.com>
2021-12-12 22:56:15 +0900

    not so important

--- db/mysql_init/01_insert_data.sql
+++ db/mysql_init/01_insert_data.sql
@@ -3,8 +3,8 @@
 INSERT INTO users 
 (id, name, email,password, nickname, first_name, last_name, delete_flg, group_id, created_at, updated_at) 
 VALUES 
-(1, "Yamada", "yamada@example.com", "testpass", "yama","taro", "yamada", 0, 2, "2021-12-01", "2021-12-01"),
-(2, "Yoshida", "yoshida@example.com", "testpass", "yoshi", "go", "yoshida", 0, 2, "2021-12-01", "2021-12-01"),
-(3, "Kawata", "kawata@example.com", "testpass", "kawa", "ken", "kawata", 0, 2, "2021-12-01", "2021-12-01"),
-(4, "Hama", "hama@example.com", "testpass", "hana", "hanako", "hama", 0, 2, "2021-12-01", "2021-12-01")
+(1, "Yamada", "yamada@example.com", "testpass", "yama","taro", "yamada", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00"),
+(2, "Yoshida", "yoshida@example.com", "testpass", "yoshi", "go", "yoshida", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00"),
+(3, "Kawata", "kawata@example.com", "testpass", "kawa", "ken", "kawata", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00"),
+(4, "Hama", "hama@example.com", "testpass", "hana", "hanako", "hama", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00")
 ;
