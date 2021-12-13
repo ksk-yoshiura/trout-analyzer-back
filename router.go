a6475be71506085a081720439f1ea30ff032adb9
keisuke <mrtambourineman8358@gmail.com>
2021-12-14 00:47:40 +0900

    router

--- router.go
+++ router.go
@@ -4,77 +4,9 @@ import (
 	"github.com/labstack/echo"
 	// "github.com/labstack/echo/middleware"
 	// "github.com/x-color/simple-webapp/handler"
-	"net/http"
+	"trout-analyzer-back/controllers"
 	"trout-analyzer-back/database"
 )
-
-type User struct {
-	Id        int    `json:"id"`
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	Nickname  string `json:"nickname"`
-	FirstName string `json:"first_name"`
-	LasttName string `json:"last_name"`
-	DeleteFlg string `json:"delete_flg"`
-	Group_id  int    `json:"group_id"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-}
-
-/**
-  ユーザ一覧取得
-*/
-func getUsers(c echo.Context) error {
-	users := []User{}
-	database.DB.Find(&users)
-	return c.JSON(http.StatusOK, users)
-}
-
-/**
-  ユーザ取得
-*/
-func getUser(c echo.Context) error {
-	user := User{}
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-	database.DB.Take(&user)
-	return c.JSON(http.StatusOK, user)
-}
-
-/**
-  ユーザ更新
-*/
-func updateUser(c echo.Context) error {
-	user := User{}
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-	database.DB.Save(&user)
-	return c.JSON(http.StatusOK, user)
-}
-
-/**
-  ユーザ作成
-*/
-func createUser(c echo.Context) error {
-	user := User{}
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-	database.DB.Create(&user)
-	return c.JSON(http.StatusCreated, user)
-}
-
-/**
-  ユーザ削除
-*/
-func deleteUser(c echo.Context) error {
-	id := c.Param("id")
-	database.DB.Delete(&User{}, id)
-	return c.NoContent(http.StatusNoContent)
-}
 
 /**
   ルート定義
@@ -85,10 +17,12 @@ func newRouter() *echo.Echo {
 	// sqlDB, _ := database.DB.DB()
 	// defer sqlDB.Close()
 
-	e.GET("/users", getUsers)
-	e.GET("/user/:id", getUser)
-	e.PUT("/user/:id", updateUser)
-	e.POST("/user", createUser)
-	e.DELETE("/user/:id", deleteUser)
+	usersController := controllers.NewUsersController()
+
+	e.GET("/users", usersController.Index)
+	// e.GET("/user/:id", getUser)
+	// e.PUT("/user/:id", updateUser)
+	// e.POST("/user", createUser)
+	// e.DELETE("/user/:id", deleteUser)
 	return e
 }
