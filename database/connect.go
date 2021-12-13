2f89f85c92fe746f3f9d5932e8eece9d2e3abffa
keisuke <mrtambourineman8358@gmail.com>
2021-12-14 00:47:07 +0900

    method

--- database/connect.go
+++ database/connect.go
@@ -1,32 +1,37 @@
 package database
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"os"
 
-  "github.com/joho/godotenv"
-  "gorm.io/driver/mysql"
-  "gorm.io/gorm"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // DBを使い回すことで、DBへのConnectとCloseを毎回しないようにする
 var DB *gorm.DB
 
 func Connect() {
-  err := godotenv.Load()
-  if err != nil {
-    panic(err.Error())
-  }
+	err := godotenv.Load()
+	if err != nil {
+		panic(err.Error())
+	}
 
-  user := os.Getenv("DB_USER")
-  password := os.Getenv("DB_PASSWORD")
-  host := os.Getenv("DB_HOST")
-  port := os.Getenv("DB_PORT")
-  database_name := os.Getenv("DB_DATABASE_NAME")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database_name := os.Getenv("DB_DATABASE_NAME")
 
-  dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4"
-  DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-  if err != nil {
-    fmt.Println(err.Error())
-  }
+	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+// GetDB returns database connection
+func GetDB() *gorm.DB {
+	return DB
 }
