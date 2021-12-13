ab6124bd89ae72c209483668969747bcf3174f7b
keisuke <mrtambourineman8358@gmail.com>
2021-12-14 00:48:17 +0900

    models folder

---
+++ models/user.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"trout-analyzer-back/database"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Nickname  string `json:"nickname"`
+	FirstName string `json:"first_name"`
+	LasttName string `json:"last_name"`
+	DeleteFlg string `json:"delete_flg"`
+	Group_id  int    `json:"group_id"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+var users []User
+
+/** ユーザー一覧取得 */
+func (u *User) GetUsers() (err error) {
+	db := database.GetDB()
+	return db.Find(&users).Error
+}
+
+/** ユーザー作成 */
+func (u *User) Create() (err error) {
+	db := database.GetDB()
+	return db.Create(u).Error
+}
+
+/** ユーザー取得 */
+func (u *User) FindByID(id uint) (err error) {
+	db := database.GetDB()
+	return db.Where("id = ?", id).First(u).Error
+}
