b60c3af4eb6c473693879ba6e83e886ffac6437b
keisuke <mrtambourineman8358@gmail.com>
2022-01-11 01:13:42 +0900

    added deleted at

--- db/mysql_init/01_insert_data.sql
+++ db/mysql_init/01_insert_data.sql
@@ -1,10 +1,10 @@
 
 -- ユーザー --
 INSERT INTO users 
-(id, name, email,password, nickname, first_name, last_name, delete_flg, group_id, created_at, updated_at) 
+(id, name, email,password, nickname, first_name, last_name, delete_flg, group_id, created_at, updated_at, deleted_at) 
 VALUES 
-(1, "Yamada", "yamada@example.com", "testpass", "yama","taro", "yamada", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00"),
-(2, "Yoshida", "yoshida@example.com", "testpass", "yoshi", "go", "yoshida", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00"),
-(3, "Kawata", "kawata@example.com", "testpass", "kawa", "ken", "kawata", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00"),
-(4, "Hama", "hama@example.com", "testpass", "hana", "hanako", "hama", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00")
+(1, "Yamada", "yamada@example.com", "testpass", "yama","taro", "yamada", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(2, "Yoshida", "yoshida@example.com", "testpass", "yoshi", "go", "yoshida", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(3, "Kawata", "kawata@example.com", "testpass", "kawa", "ken", "kawata", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(4, "Hama", "hama@example.com", "testpass", "hana", "hanako", "hama", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null)
 ;
