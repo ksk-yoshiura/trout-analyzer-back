26bc4d092528b7082b0caf6660b95299283dc3bd
keisuke <mrtambourineman8358@gmail.com>
2022-01-15 23:06:35 +0900

    field crud

---
+++ controllers/fields_controller.go
@@ -0,0 +1,123 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"trout-analyzer-back/models"
+
+	"github.com/labstack/echo"
+
+	"trout-analyzer-back/database"
+)
+
+// FieldsController controller for Fields request
+type FieldsController struct{}
+
+// NewFieldsController is constructer for FieldsController
+func NewFieldsController() *FieldsController {
+	return new(FieldsController)
+}
+
+/**
+  フィールド一覧取得
+*/
+func (uc *FieldsController) GetAllFields(c echo.Context) error {
+	db := database.GetDBConn()
+	fields := []models.Field{}
+	db.Find(&fields)
+
+	return c.JSON(http.StatusOK, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		fields,
+	))
+}
+
+/**
+  フィールド取得
+*/
+func (uc *FieldsController) GetField(c echo.Context) error {
+	db := database.GetDBConn()
+	field := models.Field{}
+	uid, _ := strconv.Atoi(c.Param("id"))
+	db.First(&field, uid)
+
+	return c.JSON(http.StatusOK, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		field,
+	))
+}
+
+/**
+  フィールド更新
+*/
+func (uc *FieldsController) UpdateField(c echo.Context) error {
+	db := database.GetDBConn()
+	field := models.Field{}
+
+	uid, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.ErrNotFound
+	}
+
+	db.First(&field, uid)
+	name := c.FormValue("name")
+	user_id, _ := strconv.Atoi(c.FormValue("user_id"))
+	address := c.FormValue("address")
+	deleteflg, _ := strconv.Atoi(c.FormValue("delete_flg"))
+
+	db.Model(&field).Updates(models.Field{
+		Name:      name,
+		UserId:    user_id,
+		Address:   address,
+		DeleteFlg: deleteflg,
+	})
+
+	return c.JSON(http.StatusOK, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		field,
+	))
+}
+
+/**
+  フィールド作成
+*/
+func (uc *FieldsController) CreateField(c echo.Context) error {
+	db := database.GetDBConn()
+	field := models.Field{}
+	if err := c.Bind(&field); err != nil {
+		return err
+	}
+
+	result := db.Create(&field).Error
+
+	return c.JSON(http.StatusCreated, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		result,
+	))
+}
+
+/**
+  フィールド削除
+*/
+func (uc *FieldsController) DeleteField(c echo.Context) error {
+	db := database.GetDBConn()
+	field := models.Field{}
+	uid, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.ErrNotFound
+	}
+
+	db.First(&field, uid)
+	result := db.Delete(&field)
+
+	return c.JSON(http.StatusOK, newResponse(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		result,
+	))
+}
---
+++ models/field.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"regexp"
+
+	"github.com/jinzhu/gorm"
+	"github.com/wcl48/valval"
+)
+
+type Field struct {
+	gorm.Model
+	Name      string `json:"name"`
+	UserId    int    `json:"user_id"`
+	Address   string `json:"address"`
+	DeleteFlg int    `json:"delete_flg"`
+}
+
+func FieldValidate(field Field) error {
+	Validator := valval.Object(valval.M{
+		"Name": valval.String(
+			valval.MaxLength(20),
+			valval.Regexp(regexp.MustCompile(`^[a-z ]+$`)),
+		),
+	})
+
+	return Validator.Validate(field)
+}
