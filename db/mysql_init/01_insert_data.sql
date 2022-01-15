45035e1436e74fc074b4cc2d5341a09eeaad0cbb
keisuke <mrtambourineman8358@gmail.com>
2022-01-15 21:47:53 +0900

    lure type needs init data

--- db/mysql_init/01_insert_data.sql
+++ db/mysql_init/01_insert_data.sql
@@ -1,10 +1,20 @@
-
+-- 初期データ --
 -- ユーザー --
 INSERT INTO users 
-(id, name, email,password, nickname, first_name, last_name, delete_flg, group_id, created_at, updated_at, deleted_at) 
+(`id`, `name`, `email`, `password`, `nickname`, `first_name`, `last_name`, `delete_flg`, `group_id`, `created_at`, `updated_at`, `deleted_at`) 
 VALUES 
 (1, "Yamada", "yamada@example.com", "testpass", "yama","taro", "yamada", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
 (2, "Yoshida", "yoshida@example.com", "testpass", "yoshi", "go", "yoshida", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
 (3, "Kawata", "kawata@example.com", "testpass", "kawa", "ken", "kawata", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
 (4, "Hama", "hama@example.com", "testpass", "hana", "hanako", "hama", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null)
+;
+
+-- ルアータイプ --
+INSERT INTO lure_types 
+(`id`, `type_name`, `delete_flg`, `created_at`, `updated_at`, `deleted_at`) 
+VALUES 
+(1, "spoon",  0, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(2, "crank-bait", 0, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(3, "minor", 0, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(4, "viberation", 0, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null)
 ;
