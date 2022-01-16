e3a3ef3ef6dceb5e73dd09cbe11b3d37274b2391
keisuke <mrtambourineman8358@gmail.com>
2022-01-16 23:33:49 +0900

    unnecessary delete flag

--- db/mysql_init/01_insert_data.sql
+++ db/mysql_init/01_insert_data.sql
@@ -1,22 +1,22 @@
 -- 初期データ --
 -- ユーザー --
 INSERT INTO users 
-(`id`, `name`, `email`, `password`, `nickname`, `first_name`, `last_name`, `delete_flg`, `group_id`, `created_at`, `updated_at`, `deleted_at`) 
+(`id`, `name`, `email`, `password`, `nickname`, `first_name`, `last_name`, `group_id`, `created_at`, `updated_at`, `deleted_at`) 
 VALUES 
-(1, "Yamada", "yamada@example.com", "testpass", "yama","taro", "yamada", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
-(2, "Yoshida", "yoshida@example.com", "testpass", "yoshi", "go", "yoshida", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
-(3, "Kawata", "kawata@example.com", "testpass", "kawa", "ken", "kawata", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
-(4, "Hama", "hama@example.com", "testpass", "hana", "hanako", "hama", 0, 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null)
+(1, "Yamada", "yamada@example.com", "testpass", "yama","taro", "yamada", 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(2, "Yoshida", "yoshida@example.com", "testpass", "yoshi", "go", "yoshida", 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(3, "Kawata", "kawata@example.com", "testpass", "kawa", "ken", "kawata", 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(4, "Hama", "hama@example.com", "testpass", "hana", "hanako", "hama", 2, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null)
 ;
 
 -- ルアータイプ --
-INSERT INTO pattern_conditions 
-(`id`, `type_name`, `delete_flg`, `created_at`, `updated_at`, `deleted_at`) 
+INSERT INTO lure_types 
+(`id`, `type_name`, `created_at`, `updated_at`, `deleted_at`) 
 VALUES 
-(1, "spoon",  0, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
-(2, "crank-bait", 0, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
-(3, "minor", 0, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
-(4, "viberation", 0, "2021-12-01 00:00:00", "2021-12-01 00:00:00", null)
+(1, "spoon",  "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(2, "crank-bait", "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(3, "minor", "2021-12-01 00:00:00", "2021-12-01 00:00:00", null),
+(4, "viberation", "2021-12-01 00:00:00", "2021-12-01 00:00:00", null)
 ;
 
 -- ヒットパターン各条件 --
