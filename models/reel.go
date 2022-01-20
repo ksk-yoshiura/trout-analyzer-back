3f1ed51fb9233abb652b9a510b7807b1e01a0006
keisuke <mrtambourineman8358@gmail.com>
2022-01-20 21:22:08 +0900

    reel methods

--- models/reel.go
+++ models/reel.go
@@ -2,8 +2,11 @@ package models
 
 import (
 	"regexp"
+	"strconv"
+	"trout-analyzer-back/database"
 
 	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
 	"github.com/wcl48/valval"
 )
 
@@ -25,4 +28,64 @@ func ReelValidate(reel Reel) error {
 	})
 
 	return Validator.Validate(reel)
+}
+
+/**
+  リール一覧取得
+*/
+func GetAllReels(reels []Reel) []Reel {
+	db := database.GetDBConn()
+	db.Find(&reels)
+	return reels
+}
+
+/**
+  リール取得
+*/
+func GetReel(reel Reel, reel_id int) Reel {
+	db := database.GetDBConn()
+	db.First(&reel, reel_id)
+	return reel
+}
+
+/**
+  リール更新
+*/
+func UpdateReel(reel Reel, reel_id int, c echo.Context) error {
+	db := database.GetDBConn()
+
+	db.First(&reel, reel_id)
+	name := c.FormValue("name")
+	user_id, _ := strconv.Atoi(c.FormValue("user_id"))
+	type_number, _ := strconv.Atoi(c.FormValue("type_number"))
+	gear := c.FormValue("gear")
+	company_name := c.FormValue("company_name")
+
+	result := db.Model(&reel).Updates(Reel{
+		Name:        name,
+		UserId:      user_id,
+		TypeNumber:  type_number,
+		Gear:        gear,
+		CompanyName: company_name,
+	}).Error
+	return result
+}
+
+/**
+  リール作成
+*/
+func CreateReel(reel Reel) error {
+	db := database.GetDBConn()
+	result := db.Create(&reel).Error
+	return result
+}
+
+/**
+  リール削除
+*/
+func DeleteReel(reel Reel, reel_id int) error {
+	db := database.GetDBConn()
+	db.First(&reel, reel_id)
+	result := db.Delete(&reel).Error
+	return result
 }
