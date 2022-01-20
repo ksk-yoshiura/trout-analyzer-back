b695d549a2ece3cd2df757f5dff519199a084058
keisuke <mrtambourineman8358@gmail.com>
2022-01-20 22:05:45 +0900

    rod methods in model

--- models/rod.go
+++ models/rod.go
@@ -2,8 +2,11 @@ package models
 
 import (
 	"regexp"
+	"strconv"
+	"trout-analyzer-back/database"
 
 	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
 	"github.com/wcl48/valval"
 )
 
@@ -25,4 +28,64 @@ func RodValidate(rod Rod) error {
 	})
 
 	return Validator.Validate(rod)
+}
+
+/**
+  ロッド一覧取得
+*/
+func GetAllRods(rods []Rod) []Rod {
+	db := database.GetDBConn()
+	db.Find(&rods)
+	return rods
+}
+
+/**
+  ロッド取得
+*/
+func GetRod(rod Rod, rod_id int) Rod {
+	db := database.GetDBConn()
+	db.First(&rod, rod_id)
+	return rod
+}
+
+/**
+  ロッド更新
+*/
+func UpdateRod(rod Rod, rod_id int, c echo.Context) error {
+	db := database.GetDBConn()
+
+	db.First(&rod, rod_id)
+	name := c.FormValue("name")
+	user_id, _ := strconv.Atoi(c.FormValue("user_id"))
+	hardness_id, _ := strconv.Atoi(c.FormValue("hardness_id"))
+	length := c.FormValue("length")
+	company_name := c.FormValue("company_name")
+
+	result := db.Model(&rod).Updates(Rod{
+		Name:        name,
+		UserId:      user_id,
+		HardnessId:  hardness_id,
+		Length:      length,
+		CompanyName: company_name,
+	}).Error
+	return result
+}
+
+/**
+  ロッド作成
+*/
+func CreateRod(rod Rod) error {
+	db := database.GetDBConn()
+	result := db.Create(&rod).Error
+	return result
+}
+
+/**
+  ロッド削除
+*/
+func DeleteRod(rod Rod, rod_id int) error {
+	db := database.GetDBConn()
+	db.First(&rod, rod_id)
+	result := db.Delete(&rod).Error
+	return result
 }
