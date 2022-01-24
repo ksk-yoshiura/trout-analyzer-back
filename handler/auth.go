ae053e5ced13a77576982b0717973f6c59e1909e
keisuke <mrtambourineman8358@gmail.com>
2022-01-25 00:09:19 +0900

    only login is OK

---
+++ handler/auth.go
@@ -0,0 +1,92 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+
+	"trout-analyzer-back/models"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type jwtCustomClaims struct {
+	UID   int    `json:"uid"`
+	Email string `json:"email"`
+
+	jwt.StandardClaims
+}
+
+var signingKey = []byte("secret")
+
+var Config = middleware.JWTConfig{
+	Claims:     &jwtCustomClaims{},
+	SigningKey: signingKey,
+}
+
+func Signup(c echo.Context) error {
+	user := models.User{}
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+
+	if user.Email == "" || user.Password == "" {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: "invalid email or password",
+		}
+	}
+
+	if u := models.FindUser(models.User{Email: user.Email}); u.ID != 0 {
+		return &echo.HTTPError{
+			Code:    http.StatusConflict,
+			Message: "email already exists",
+		}
+	}
+
+	models.CreateUser(user)
+	user.Password = ""
+
+	return c.JSON(http.StatusCreated, user)
+}
+
+func Login(c echo.Context) error {
+	u := models.User{}
+	if err := c.Bind(&u); err != nil {
+		return err
+	}
+
+	user := models.FindUser(models.User{Email: u.Email})
+	if user.ID == 0 || user.Password != u.Password {
+		return &echo.HTTPError{
+			Code:    http.StatusUnauthorized,
+			Message: "invalid email or password",
+		}
+	}
+
+	claims := &jwtCustomClaims{
+		int(user.ID),
+		user.Email,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString(signingKey)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": t,
+	})
+}
+
+func userIDFromToken(c echo.Context) int {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*jwtCustomClaims)
+	uid := claims.UID
+	return uid
+}
