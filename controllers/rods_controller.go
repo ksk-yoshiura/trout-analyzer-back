fbfe47a06956d085413d9db5a3547899b843aeb3
keisuke <mrtambourineman8358@gmail.com>
2022-01-26 01:12:08 +0900

    left comment and change order of logic

--- controllers/rods_controller.go
+++ controllers/rods_controller.go
@@ -21,6 +21,7 @@ func NewRodsController() *RodsController {
   ロッド一覧取得
 */
 func (uc *RodsController) Index(c echo.Context) error {
+	// データ取得
 	rods := []models.Rod{}
 	result := models.GetAllRods(rods)
 
@@ -35,12 +36,14 @@ func (uc *RodsController) Index(c echo.Context) error {
   ロッド取得
 */
 func (uc *RodsController) Show(c echo.Context) error {
-	rod := models.Rod{}
+	// idチェック
 	rod_id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		return echo.ErrNotFound
 	}
 
+	// データ取得
+	rod := models.Rod{}
 	result := models.GetRod(rod, rod_id)
 
 	return c.JSON(http.StatusOK, newResponse(
@@ -54,18 +57,22 @@ func (uc *RodsController) Show(c echo.Context) error {
   ロッド更新
 */
 func (uc *RodsController) Update(c echo.Context) error {
-	rod := models.Rod{}
-	if err := c.Bind(&rod); err != nil {
-		return err
-	}
-
+	// idチェック
 	rod_id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		return echo.ErrNotFound
 	}
 
+	// データセット
+	rod := models.Rod{}
+	if err := c.Bind(&rod); err != nil {
+		return err
+	}
+
+	// トークンからユーザID取得
 	uid := userIDFromToken(c)
 
+	// 更新
 	result := models.UpdateRod(rod, rod_id, uid)
 
 	return c.JSON(http.StatusOK, newResponse(
@@ -79,14 +86,17 @@ func (uc *RodsController) Update(c echo.Context) error {
   ロッド作成
 */
 func (uc *RodsController) Create(c echo.Context) error {
+	// データセット
 	rod := models.Rod{}
 	if err := c.Bind(&rod); err != nil {
 		return err
 	}
 
+	// トークンからユーザID取得
 	uid := userIDFromToken(c)
 	rod.UserId = uid
 
+	// 登録
 	result := models.CreateRod(rod)
 
 	return c.JSON(http.StatusCreated, newResponse(
@@ -100,12 +110,14 @@ func (uc *RodsController) Create(c echo.Context) error {
   ロッド削除
 */
 func (uc *RodsController) Delete(c echo.Context) error {
-	rod := models.Rod{}
+	// idチェック
 	rod_id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		return echo.ErrNotFound
 	}
 
+	// 削除
+	rod := models.Rod{}
 	result := models.DeleteRod(rod, rod_id)
 
 	return c.JSON(http.StatusOK, newResponse(
