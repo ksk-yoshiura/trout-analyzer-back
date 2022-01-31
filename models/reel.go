fab7b921058177d81e98d446293d954ab6642eeb
keisuke <mrtambourineman8358@gmail.com>
2022-01-31 22:21:45 +0900

    show association

--- models/reel.go
+++ models/reel.go
@@ -46,7 +46,7 @@ func GetAllReels(reels []Reel, uid int) []Reel {
 func GetReel(reel Reel, reel_id int, uid int) Reel {
 	db := database.GetDBConn()
 	// ログインユーザは自分のリールしか見れない
-	db.Where("user_id = ?", uid).First(&reel, reel_id)
+	db.Where("user_id = ?", uid).Preload("GearCondition").Preload("TypeNumberCondition").First(&reel, reel_id)
 	return reel
 }
 
