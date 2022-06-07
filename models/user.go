9f02cc6e5f8fc122ff998f873e4ad2391823ae84
keisuke <mrtambourineman8358@gmail.com>
2022-06-07 22:51:54 +0900

    removed valval

--- models/user.go
+++ models/user.go
@@ -1,10 +1,8 @@
 package models
 
 import (
-	"regexp"
 	"trout-analyzer-back/database"
 
-	"github.com/wcl48/valval"
 	"gorm.io/gorm"
 )
 
@@ -16,17 +14,6 @@ type User struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	GroupId   int    `json:"groupId"`
-}
-
-func UserValidate(user User) error {
-	Validator := valval.Object(valval.M{
-		"Name": valval.String(
-			valval.MaxLength(20),
-			valval.Regexp(regexp.MustCompile(`^[a-z ]+$`)),
-		),
-	})
-
-	return Validator.Validate(user)
 }
 
 /**
