66026e5e721751108c59dd2761dd3105a42330e2
keisuke <mrtambourineman8358@gmail.com>
2022-07-12 23:50:22 +0900

    iam for ecr

--- terraform/cicd/tranaza/iam.tf
+++ terraform/cicd/tranaza/iam.tf
@@ -6,29 +6,37 @@ resource "aws_iam_user" "github" {
   }
 }
 
-# resource "aws_iam_role" "deployer" {
-#   name = "tranaza-prod-deployer"
+resource "aws_iam_role" "deployer" {
+  name = "tranaza-prod-deployer"
 
-#   assume_role_policy = jsonencode(
-#     {
-#       "Version" : "2012-10-17",
-#       "Statement" : [
-#         {
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "sts:AssumeRole",
-#             "sts:TagSession"
-#           ],
-#           "Pricipal": {
-#             "AWS": aws_iam_user.github.arn
-#           }
-#         }
-#       ]
-#     }
-#   )
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "sts:AssumeRole",
+            "sts:TagSession"
+          ],
+          "Principal": {
+            "AWS": aws_iam_user.github.arn
+          }
+        }
+      ]
+    }
+  )
 
-#   tags = {
-#     Name = "${locals.name_prefix}-${locals.service_name}-deployer"
-#   }
-# }
+  tags = {
+    Name = "tranaza-prod-deployer"
+  }
+}
 
+data "aws_iam_policy" "ecr_power_user" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role_policy_attachment" "role_deployer_policy_ecr_power_user" {
+  role = aws_iam_role.deployer.name
+  policy_arn = data.aws_iam_policy.ecr_power_user.arn
+}
