64d26c7c490ba2d549a363078eedccdf776736a4
keisuke <mrtambourineman8358@gmail.com>
2022-07-14 23:21:42 +0900

    vpc terraform

---
+++ terraform/network/main/eip.tf
@@ -0,0 +1,11 @@
+resource "aws_eip" "nat_gateway" {
+  for_each = var.enable_nat_gateway ? local.nat_gateway_azs : {}
+  vpc = true
+  tags = {
+    Name = "${aws_vpc.this.tags.Name}-nat-gateway-${each.key}"
+  }
+}
+
+locals { // コスト戦略
+  nat_gateway_azs = var.single_nat_gateway ? {keys(var.azs)[0] = values(var.azs)[0]} : var.azs
+}
---
+++ terraform/network/main/variables.tf
@@ -0,0 +1,31 @@
+variable "vpc_cidr" {
+  type = string
+  default = "172.31.0.0/16"
+}
+
+variable "azs" {
+  type = map(object({
+    public_cidr = string
+    private_cidr = string
+  }))
+  default = {
+    a = {
+      public_cidr = "172.31.0.0/20"
+      private_cidr = "172.31.48.0/20"
+    },
+    c = {
+      public_cidr = "172.31.16.0/20"
+      private_cidr = "172.31.64.0/20"
+    }
+  }
+}
+
+variable "enable_nat_gateway" {
+  type = bool
+  default = true
+}
+
+variable "single_nat_gateway" {
+  type = bool
+  default = true // コスト重視
+}
