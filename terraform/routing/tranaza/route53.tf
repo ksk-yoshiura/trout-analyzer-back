2525414e0ef5e8933343c002dbf8039192c33a2a
keisuke <mrtambourineman8358@gmail.com>
2022-07-16 23:08:01 +0900

    acm terraform

---
+++ terraform/routing/tranaza/route53.tf
@@ -0,0 +1,19 @@
+data "aws_route53_zone" "this" {
+  name = "tranaza.link"
+}
+
+resource "aws_route53_zone" "certificate_validation" {
+  for_each = {
+    for dvo in aws_acm_certification.root.domain_validation_options : dvo.domain_name => {
+      name = dvo.resource_record_name
+      type = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  name = each.value.name
+  records = [each.value.record]
+  ttl = 60
+  type = each.value.type
+  zone_id = data.aws_route53_zone.this.id
+}
