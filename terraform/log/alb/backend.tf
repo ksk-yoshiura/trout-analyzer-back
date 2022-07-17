45abb7b9fb63730a023c12217da784c57268cf0c
keisuke <mrtambourineman8358@gmail.com>
2022-07-17 15:51:04 +0900

    fixed

--- terraform/log/alb/backend.tf
+++ terraform/log/alb/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tfstate-s3-bucket-for-tranaza"
-    key    = "log/alb/tranaza_v1.2.4.tfstate"
+    key    = "log/alb_v1.2.4.tfstate"
     region = "ap-northeast-1"
   }
 }
--- terraform/log/alb/data.tf
+++ terraform/log/alb/data.tf
@@ -1,5 +1,1 @@
 data "aws_elb_service_account" "current" {}
-
-output "s3_bucket_this_id" {
-  value = aws_s3_bucket.this.id
-}
