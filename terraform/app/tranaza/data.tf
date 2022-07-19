ace04239e3a678f2460e42a168304a55c268e82e
keisuke <mrtambourineman8358@gmail.com>
2022-07-19 23:26:00 +0900

    ecs cluster

--- terraform/app/tranaza/data.tf
+++ terraform/app/tranaza/data.tf
@@ -1,3 +1,24 @@
 data "aws_caller_identity" "self" {}
 
 data "aws_region" "current" {}
+
+data "terraform_remote_state" "network_main" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-s3-bucket-for-tranaza"
+    key    = "network/main_v1.2.4.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+data "terraform_remote_state" "routing_tranaza_link" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-s3-bucket-for-tranaza"
+    key    = "routing/tranaza_v1.2.4.tfstate"
+    region = "ap-northeast-1"
+  }
+
+}
