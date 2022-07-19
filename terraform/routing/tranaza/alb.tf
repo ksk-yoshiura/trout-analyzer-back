35c18d30b2f96d954bba03a71004d20b9fb768cc
keisuke <mrtambourineman8358@gmail.com>
2022-07-19 22:49:24 +0900

    target group terraform

--- terraform/routing/tranaza/alb.tf
+++ terraform/routing/tranaza/alb.tf
@@ -3,13 +3,13 @@ resource "aws_lb" "this" {
 
   name = "${local.name_prefix}-tranaza"
 
-  internal = false
+  internal           = false
   load_balancer_type = "application"
 
   access_logs {
-    bucket = data.terraform_remote_state.log_alb.outputs.s3_bucket_this_id
+    bucket  = data.terraform_remote_state.log_alb.outputs.s3_bucket_this_id
     enabled = true
-    prefix = "tranaza"
+    prefix  = "tranaza"
   }
 
   security_groups = [
@@ -27,41 +27,62 @@ resource "aws_lb" "this" {
 }
 
 resource "aws_lb_listener" "https" {
-  count  = var.enable_alb ? 1 : 0 
-  
-  certificate_arn = aws_acm_certificate.root.arn
+  count = var.enable_alb ? 1 : 0
+
+  certificate_arn   = aws_acm_certificate.root.arn
   load_balancer_arn = aws_lb.this[0].arn
-  port = "443"
-  protocol = "HTTPS"
+  port              = "443"
+  protocol          = "HTTPS"
 
   ssl_policy = "ELBSecurityPolicy-2016-08"
 
   default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code = "200"
-    }
+    type = "forward"
+    target_group_arn = aws_lb_target_group.tranaza.arn
   }
+
 }
 
 resource "aws_lb_listener" "rredirect_http_to_https" {
-  count  = var.enable_alb ? 1 : 0 
-  
+  count = var.enable_alb ? 1 : 0
+
   load_balancer_arn = aws_lb.this[0].arn
-  port = "80"
-  protocol = "HTTP"
+  port              = "80"
+  protocol          = "HTTP"
 
 
   default_action {
     type = "redirect"
 
     redirect {
-      port = "443"
-      protocol = "HTTPS"
+      port        = "443"
+      protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
+  }
+}
+
+resource "aws_lb_target_group" "tranaza" {
+  name = "${local.name_prefix}-tranaza"
+
+  deregistration_delay = 60
+  port                  = 80
+  protocol              = "HTTP"
+  target_type           = "ip"
+  vpc_id                = data.terraform_remote_state.network_main.outputs.vpc_this_id
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = 200
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${local.name_prefix}-tranaza"
   }
 }
---
+++ terraform/routing/tranaza/outputs.tf
@@ -0,0 +1,3 @@
+output "lb_target_group_tranaza_arn" {
+  value = aws_lb_target_group.tranaza.arn
+}
