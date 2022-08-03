1e5695e5497269d5b3093d5a990865ceee46c8e7
keisuke <mrtambourineman8358@gmail.com>
2022-08-03 23:28:43 +0900

    iam

--- terraform/app/tranaza/iam.tf
+++ terraform/app/tranaza/iam.tf
@@ -30,6 +30,28 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   policy_arn = data.aws_iam_policy.ecs_task_execution.arn
 }
 
+resource "aws_iam_role" "ecs_task" {
+  name = "${local.name_prefix}-${local.service_name}-ecs-task"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ecs-tasks.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
+    }
+  )
+  tags = {
+    Name = "${local.name_prefix}-${local.service_name}-ecs-task"
+  }
+}
+
 # resource "aws_iam_policy" "ssm" { // 多分関係ない。念の為残す
 #   name = "${local.name_prefix}-${local.service_name}-ssm"
 #   policy = jsonencode(
@@ -56,28 +78,6 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
 # resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm" {
 #   role       = aws_iam_role.ecs_task_execution.name
 #   policy_arn = aws_iam_policy.ssm.arn
-# }
-
-# resource "aws_iam_role" "ecs_task" {
-#   name = "${local.name_prefix}-${local.service_name}-ecs-task"
-
-#   assume_role_policy = jsonencode(
-#     {
-#       "Version" : "2012-10-17",
-#       "Statement" : [
-#         {
-#           "Effect" : "Allow",
-#           "Principal" : {
-#             "Service" : "ecs-tasks.amazonaws.com"
-#           },
-#           "Action" : "sts:AssumeRole"
-#         }
-#       ]
-#     }
-#   )
-#   tags = {
-#     Name = "${local.name_prefix}-${local.service_name}-ecs-task"
-#   }
 # }
 
 # resource "aws_iam_role_policy" "ecs_task_ssm" {
