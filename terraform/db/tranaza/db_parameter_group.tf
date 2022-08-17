33b3d7a7d0e801f70a5481a6d885f30e5f66be47
keisuke <mrtambourineman8358@gmail.com>
2022-08-17 23:33:31 +0900

    db

---
+++ terraform/db/tranaza/db_parameter_group.tf
@@ -0,0 +1,65 @@
+resource "aws_db_parameter_group" "this" {
+  name = "${local.service_name}-${local.env_name}-${local.service_name}"
+
+  family = "mysql8.0"
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_connection"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_filesystem"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_results"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_0900ai_ci"
+  }
+
+  parameter {
+    name  = "general_log"
+    value = "1"
+  }
+
+
+  parameter {
+    name  = "slow_query_log"
+    value = "1"
+  }
+
+  parameter {
+    name  = "long_query_log"
+    value = "1.o"
+  }
+
+  parameter {
+    name  = "log_output"
+    value = "FILE"
+  }
+
+  tags = {
+    Name = "${local.service_name}-${local.env_name}-${local.service_name}"
+  }
+}
