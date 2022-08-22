d7f583f1c7a145d8babee0a5adfd35b653142d13
keisuke <mrtambourineman8358@gmail.com>
2022-08-23 00:47:35 +0900

    task unnecessary

--- terraform/app/tranaza/ecs.tf
+++ terraform/app/tranaza/ecs.tf
@@ -10,118 +10,118 @@ resource "aws_ecs_cluster" "this" {
   }
 }
 
-resource "aws_ecs_task_definition" "this" {
-  family = "${local.name_prefix}-${local.service_name}"
+# resource "aws_ecs_task_definition" "this" {
+#   family = "${local.name_prefix}-${local.service_name}"
 
-  task_role_arn = aws_iam_role.ecs_task.arn
+#   task_role_arn = aws_iam_role.ecs_task.arn
 
-  network_mode = "awsvpc"
+#   network_mode = "awsvpc"
 
-  requires_compatibilities = [
-    "FARGATE",
-  ]
+#   requires_compatibilities = [
+#     "FARGATE",
+#   ]
 
-  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+#   execution_role_arn = aws_iam_role.ecs_task_execution.arn
 
-  memory = "512"
-  cpu    = "256"
+#   memory = "512"
+#   cpu    = "256"
 
-  container_definitions = jsonencode(
-    [
-      {
-        name  = "nginx"
-        image = "${module.nginx.ecr_repository_this_repository_url}:latest"
+#   container_definitions = jsonencode(
+#     [
+#       {
+#         name  = "nginx"
+#         image = "${module.nginx.ecr_repository_this_repository_url}:latest"
 
-        portMappings = [
-          {
-            containerPort = 80
-            protocol      = "tcp"
-          }
-        ]
+#         portMappings = [
+#           {
+#             containerPort = 80
+#             protocol      = "tcp"
+#           }
+#         ]
 
-        dependsOn = [
-          {
-            containerName = "golang"
-            condition     = "START"
-          }
-        ]
+#         dependsOn = [
+#           {
+#             containerName = "golang"
+#             condition     = "START"
+#           }
+#         ]
 
-        logConfiguration = {
-          logDriver = "awslogs"
-          options = {
-            awslogs-group         = "/ecs/${local.service_name}-${local.env_name}-nginx"
-            awslogs-region        = data.aws_region.current.id
-            awslogs-stream-prefix = "ecs"
-          }
-        }
-      },
-      {
-        name  = "golang"
-        image = "${module.golang.ecr_repository_this_repository_url}:latest"
+#         logConfiguration = {
+#           logDriver = "awslogs"
+#           options = {
+#             awslogs-group         = "/ecs/${local.service_name}-${local.env_name}-nginx"
+#             awslogs-region        = data.aws_region.current.id
+#             awslogs-stream-prefix = "ecs"
+#           }
+#         }
+#       },
+#       {
+#         name  = "golang"
+#         image = "${module.golang.ecr_repository_this_repository_url}:latest"
 
-        portMappings : [
-          {
-            containerPort : 3000,
-            hostPort : 3000
-          }
-        ]
-        logConfiguration = {
-          logDriver = "awslogs"
-          options = {
-            awslogs-group         = "/ecs/${local.service_name}-${local.env_name}-golang"
-            awslogs-region        = data.aws_region.current.id
-            awslogs-stream-prefix = "ecs"
-          }
-        }
-      }
-    ]
-  )
+#         portMappings : [
+#           {
+#             containerPort : 3000,
+#             hostPort : 3000
+#           }
+#         ]
+#         logConfiguration = {
+#           logDriver = "awslogs"
+#           options = {
+#             awslogs-group         = "/ecs/${local.service_name}-${local.env_name}-golang"
+#             awslogs-region        = data.aws_region.current.id
+#             awslogs-stream-prefix = "ecs"
+#           }
+#         }
+#       }
+#     ]
+#   )
 
-  tags = {
-    Name = "${local.name_prefix}-${local.service_name}"
-  }
-}
+#   tags = {
+#     Name = "${local.name_prefix}-${local.service_name}"
+#   }
+# }
 
-resource "aws_ecs_service" "this" {
-  name = "${local.name_prefix}-${local.service_name}"
+# resource "aws_ecs_service" "this" {
+#   name = "${local.name_prefix}-${local.service_name}"
 
-  cluster = aws_ecs_cluster.this.arn
+#   cluster = aws_ecs_cluster.this.arn
 
-  capacity_provider_strategy {
-    capacity_provider = "FARGATE_SPOT"
-    base              = 0
-    weight            = 1
-  }
+#   capacity_provider_strategy {
+#     capacity_provider = "FARGATE_SPOT"
+#     base              = 0
+#     weight            = 1
+#   }
 
-  platform_version = "1.4.0"
+#   platform_version = "1.4.0"
 
-  task_definition = aws_ecs_task_definition.this.arn
+#   task_definition = aws_ecs_task_definition.this.arn
 
-  desired_count                      = var.desired_count
-  deployment_minimum_healthy_percent = 100
-  deployment_maximum_percent         = 200
+#   desired_count                      = var.desired_count
+#   deployment_minimum_healthy_percent = 100
+#   deployment_maximum_percent         = 200
 
-  load_balancer {
-    container_name   = "nginx"
-    container_port   = 80
-    target_group_arn = data.terraform_remote_state.routing_tranaza_link.outputs.lb_target_group_tranaza_arn
-  }
+#   load_balancer {
+#     container_name   = "nginx"
+#     container_port   = 80
+#     target_group_arn = data.terraform_remote_state.routing_tranaza_link.outputs.lb_target_group_tranaza_arn
+#   }
 
-  health_check_grace_period_seconds = 180
+#   health_check_grace_period_seconds = 180
 
-  network_configuration {
-    assign_public_ip = false
-    security_groups = [
-      data.terraform_remote_state.network_main.outputs.security_group_vpc_id
-    ]
-    subnets = [
-      for s in data.terraform_remote_state.network_main.outputs.subnet_private : s.id
-    ]
-  }
+#   network_configuration {
+#     assign_public_ip = false
+#     security_groups = [
+#       data.terraform_remote_state.network_main.outputs.security_group_vpc_id
+#     ]
+#     subnets = [
+#       for s in data.terraform_remote_state.network_main.outputs.subnet_private : s.id
+#     ]
+#   }
 
-  enable_execute_command = true
+#   enable_execute_command = true
 
-  tags = {
-    Name = "${local.name_prefix}-${local.service_name}"
-  }
-}
+#   tags = {
+#     Name = "${local.name_prefix}-${local.service_name}"
+#   }
+# }
