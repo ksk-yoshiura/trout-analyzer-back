1a8b5031d36a0571866189965841f5305cbcb8ce
keisuke <mrtambourineman8358@gmail.com>
2022-08-23 01:10:09 +0900

    cicd

--- terraform/cicd/tranaza/ecspresso.tf
+++ terraform/cicd/tranaza/ecspresso.tf
@@ -28,6 +28,10 @@ data "aws_lb_target_group" "this" {
   name = "${local.name_prefix}-${local.service_name}"
 }
 
+data "aws_security_group" "db_tranaza" {
+  name = "${local.name_prefix}-main-db-tranaza"
+}
+
 data "aws_security_group" "vpc" {
   name = "${local.name_prefix}-main-vpc"
 }
