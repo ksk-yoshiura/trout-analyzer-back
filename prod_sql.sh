be6eb4c6fd916d69e5e455d8f7a251aa2eefb5a8
keisuke <mrtambourineman8358@gmail.com>
2022-08-28 17:27:08 +0900

    env var

--- prod_sql.sh
+++ prod_sql.sh
@@ -1,1 +1,3 @@
-mysql -h 127.0.0.1 --port 3306 -u root -p DB_PASSWORD -e "./db/00_create_tables.sql"
+#!/bin/bash
+echo $DB_PASSWORD
+#mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p $DB_PASSWORD -e trout_analyzer < "./db/00_create_tables.sql"
