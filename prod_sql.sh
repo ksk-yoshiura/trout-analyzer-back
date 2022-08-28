97a9043f5f3dfbe6817971d6469206137e9da4d1
keisuke <mrtambourineman8358@gmail.com>
2022-08-29 01:11:51 +0900

    trial

--- prod_sql.sh
+++ prod_sql.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -e
-mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p $DB_PASSWORD trout_analyzer < "./db/mysql_init/00_create_tables.sql"
+HOGE=hoge
+echo $HOGE
+mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p $DB_PASSWORD  -e < "./db/mysql_init/00_create_tables.sql"
