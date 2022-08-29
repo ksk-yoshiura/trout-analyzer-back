6a424d8638410a73a460c42041b382c5f60c957c
keisuke <mrtambourineman8358@gmail.com>
2022-08-30 00:44:05 +0900

    password

--- prod_sql.sh
+++ prod_sql.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eu
 
-HOGE=hoge
-
-echo $HOGE
+$DB_PASSWORD = =(`aws ssm get-parameters --names /tranaza/DB_PASSWORD --query "Parameters[*].{Value: Value}"  --output text`)
 
 mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p $DB_PASSWORD  -eu < "./db/mysql_init/00_create_tables.sql"
