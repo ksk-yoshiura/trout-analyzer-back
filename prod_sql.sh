3fe738fc569107fe9f24514ad55a6ac7e3663f53
keisuke <mrtambourineman8358@gmail.com>
2022-08-30 23:48:22 +0900

    bash -x looks good

--- prod_sql.sh
+++ prod_sql.sh
@@ -3,6 +3,4 @@ set -eu
 
 DB_PASSWORD=(`aws ssm get-parameters --names "/tranaza/DB_PASSWORD" --query "Parameters[*].{Value: Value}" --with-decryption --output text`)
 
-echo $DB_PASSWORD
-
-mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p ${DB_PASSWORD} -eu < "./db/mysql_init/00_create_tables.sql"
+mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p"${DB_PASSWORD}" -eu < "./db/mysql_init/00_create_tables.sql"
