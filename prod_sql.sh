93283b2f88cb4d189b7ccaeabd07097a76fb116c
keisuke <mrtambourineman8358@gmail.com>
2022-08-31 23:09:51 +0900

    trial

--- prod_sql.sh
+++ prod_sql.sh
@@ -2,5 +2,5 @@
 set -eu
 
 DB_PASSWORD=(`aws ssm get-parameters --names "/tranaza/DB_PASSWORD" --query "Parameters[*].{Value: Value}" --with-decryption --output text`)
-
-mysql -h "${DB_HOST}"  --port 3306 -u tranaza -p"${DB_PASSWORD}" -eu < "./db/mysql_init/00_create_tables.sql"
+echo $DB_HOST
+mysql -h "${DB_HOST}"  --port 3306 -u tranaza -p"${DB_PASSWORD}" #-eu < "./db/mysql_init/00_create_tables.sql"
