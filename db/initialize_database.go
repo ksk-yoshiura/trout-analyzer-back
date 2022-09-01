8622664564ea10ed5aee03c704cc0ab1af5f6b07
keisuke <mrtambourineman8358@gmail.com>
2022-09-02 00:45:35 +0900

    for migration

---
+++ db/initialize_database.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func getDBConfig() string {
+	user := os.Getenv("DB_USER")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database_name := os.Getenv("DB_DATABASE_NAME")
+
+	password := os.Getenv("DB_PASSWORD")
+
+	CONNECT := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4&parseTime=true"
+
+	return CONNECT
+}
+
+func executeInitialize() error {
+	CONNECT := getDBConfig()
+	out, err := exec.Command("migrate", "-path", "migration/db/migration", "-database", CONNECT, "1", "up").Output()
+
+	if err != nil {
+		return err
+	}
+	fmt.Printf("result: %s", out)
+	return nil
+}
+
+func main() {
+	executeInitialize()
+}
