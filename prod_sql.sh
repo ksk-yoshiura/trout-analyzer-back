f5ae3fcbdd6f186eb549c1928bdf5e6e5c782480
keisuke <mrtambourineman8358@gmail.com>
2022-09-02 00:43:53 +0900

    removed

--- prod_sql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eu
-
-DB_PASSWORD=(`aws ssm get-parameters --names "/tranaza/DB_PASSWORD" --query "Parameters[*].{Value: Value}" --with-decryption --output text`)
-
-mysql -h "${DB_HOST}" --port 3306 -u tranaza -p"${DB_PASSWORD}" tranaza-prod-tranaza #-eu < "./db/mysql_init/00_create_tables.sql"
