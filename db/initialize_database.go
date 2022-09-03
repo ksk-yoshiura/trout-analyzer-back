9fbadf3437d365faa7a0ad5008ccaae74ec84df8
keisuke <mrtambourineman8358@gmail.com>
2022-09-04 00:44:52 +0900

    found install got env

--- db/initialize_database.go
+++ db/initialize_database.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	// _ "github.com/go-sql-driver/mysql"
 )
 
@@ -28,6 +32,27 @@ import (
 // 	return DB_PASSWORD
 // }
 
+func getDBConfig() string {
+	err := godotenv.Load("./backend/.env.prod")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := os.Getenv("DB_USER")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database_name := os.Getenv("DB_DATABASE_NAME")
+
+	password := "ttsttsts"
+
+	fmt.Println(password)
+
+	CONNECT := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4&parseTime=true"
+
+	return CONNECT
+}
+
 func executeInitialize() {
 	CONNECT := getDBConfig()
 
@@ -47,27 +72,6 @@ func executeInitialize() {
 	// 	fmt.Println("データベース接続成功")
 	// }
 
-}
-
-func getDBConfig() string {
-	// err := godotenv.Load("./backend/.env.prod")
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// 	user := os.Getenv("DB_USER")
-	// 	host := os.Getenv("DB_HOST")
-	// 	port := os.Getenv("DB_PORT")
-	// 	database_name := os.Getenv("DB_DATABASE_NAME")
-
-	// 	password := "ttsttsts"
-
-	// 	fmt.Println(password)
-
-	// 	CONNECT := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4&parseTime=true"
-
-	return "test method"
 }
 
 func main() {
