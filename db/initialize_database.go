3e1c7ce9aa184833e16cd40fa1fc033fdd57fc36
keisuke <mrtambourineman8358@gmail.com>
2022-09-04 01:27:26 +0900

    method

--- db/initialize_database.go
+++ db/initialize_database.go
@@ -9,28 +9,30 @@ import (
 	// _ "github.com/go-sql-driver/mysql"
 )
 
-// func getDatabasePassword() string {
-// 	// sess, err := session.NewSessionWithOptions(session.Options{
-// 	// 	Config:  aws.Config{Region: aws.String("ap-northeast-1")},
-// 	// 	Profile: "default",
-// 	// })
-// 	// if err != nil {
-// 	// 	log.Fatal(err)
-// 	// }
-// 	// svc := ssm.New(sess)
+func getDatabasePassword() string {
+	// sess, err := session.NewSessionWithOptions(session.Options{
+	// 	Config:  aws.Config{Region: aws.String("ap-northeast-1")},
+	// 	Profile: "default",
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// svc := ssm.New(sess)
 
-// 	// res, err := svc.GetParameter(&ssm.GetParameterInput{
-// 	// 	Name:           aws.String("/tranaza/DB_PASSWORD"),
-// 	// 	WithDecryption: aws.Bool(true),
-// 	// })
-// 	// if err != nil {
-// 	// 	log.Fatal(err)
-// 	// }
+	// res, err := svc.GetParameter(&ssm.GetParameterInput{
+	// 	Name:           aws.String("/tranaza/DB_PASSWORD"),
+	// 	WithDecryption: aws.Bool(true),
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-// 	DB_PASSWORD := "test" // *res.Parameter.Value
+	DB_PASSWORD := "test"
 
-// 	return DB_PASSWORD
-// }
+	// *res.Parameter.Value
+
+	return DB_PASSWORD
+}
 
 func getDBConfig() string {
 	err := godotenv.Load("./backend/.env.prod")
@@ -44,9 +46,7 @@ func getDBConfig() string {
 	port := os.Getenv("DB_PORT")
 	database_name := os.Getenv("DB_DATABASE_NAME")
 
-	password := "test"
-
-	// getDatabasePassword()
+	password := getDatabasePassword()
 
 	CONNECT := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4&parseTime=true"
 
