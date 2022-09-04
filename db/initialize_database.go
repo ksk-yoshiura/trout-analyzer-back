06ab514d9d94bf60a8ff58d8f86a9357efc7cb6f
keisuke <mrtambourineman8358@gmail.com>
2022-09-04 14:48:26 +0900

    trial

--- db/initialize_database.go
+++ db/initialize_database.go
@@ -6,6 +6,8 @@ import (
 	"os"
 
 	_ "github.com/aws/aws-sdk-go/aws"
+	_ "github.com/aws/aws-sdk-go/aws/session"
+	_ "github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/joho/godotenv"
 	// _ "github.com/go-sql-driver/mysql"
 )
