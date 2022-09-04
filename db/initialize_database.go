83459947a13c576ebf1b243d7fb38f942f4415c0
keisuke <mrtambourineman8358@gmail.com>
2022-09-04 15:05:53 +0900

    removed again

--- db/initialize_database.go
+++ db/initialize_database.go
@@ -5,24 +5,21 @@ import (
 	"log"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/joho/godotenv"
 	// _ "github.com/go-sql-driver/mysql"
 )
 
 func getDatabasePassword() string {
-	sess, err := session.NewSessionWithOptions(session.Options{
-		Config:  aws.Config{Region: aws.String("ap-northeast-1")},
-		Profile: "default",
-	})
-	if err != nil {
-		fmt.Println(err)
-		// 	// log.Fatal(err)
-	}
-	svc := ssm.New(sess)
-	fmt.Println(svc)
+	// sess, err := session.NewSessionWithOptions(session.Options{
+	// 	Config:  aws.Config{Region: aws.String("ap-northeast-1")},
+	// 	Profile: "default",
+	// })
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	// 	// log.Fatal(err)
+	// }
+	// svc := ssm.New(sess)
+	// fmt.Println(svc)
 
 	// res, err := svc.GetParameter(&ssm.GetParameterInput{
 	// 	Name:           aws.String("/tranaza/DB_PASSWORD"),
