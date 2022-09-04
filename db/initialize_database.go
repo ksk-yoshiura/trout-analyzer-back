097d8df08fdfc63a779bcffd6daed34992ec4107
keisuke <mrtambourineman8358@gmail.com>
2022-09-04 13:55:36 +0900

    comment out again

--- db/initialize_database.go
+++ db/initialize_database.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/aws/aws-sdk-go/aws"
+	// _ "github.com/aws/aws-sdk-go/aws"
 	// _ "github.com/aws/aws-sdk-go/aws/session"
 	// _ "github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/joho/godotenv"
