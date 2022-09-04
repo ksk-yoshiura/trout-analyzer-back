9982ccb5c24264bf2140af1ff97a79ec8f52d1d0
keisuke <mrtambourineman8358@gmail.com>
2022-09-04 14:52:24 +0900

    removed

--- db/initialize_database.go
+++ db/initialize_database.go
@@ -5,9 +5,6 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/aws/aws-sdk-go/aws"
-	_ "github.com/aws/aws-sdk-go/aws/session"
-	_ "github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/joho/godotenv"
 	// _ "github.com/go-sql-driver/mysql"
 )
