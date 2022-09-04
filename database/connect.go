2c1dfc68777db150af59e57794be189697693953
keisuke <mrtambourineman8358@gmail.com>
2022-09-04 14:31:38 +0900

    connect need aws sdk

--- database/connect.go
+++ database/connect.go
@@ -4,12 +4,38 @@ import (
 	"log"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+
+func getDatabasePassword() string {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config:  aws.Config{Region: aws.String("ap-northeast-1")},
+		Profile: "default",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	svc := ssm.New(sess)
+
+	res, err := svc.GetParameter(&ssm.GetParameterInput{
+		Name:           aws.String("/tranaza/DB_PASSWORD"),
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB_PASSWORD := *res.Parameter.Value
+
+	return DB_PASSWORD
+}
 
 // GetDB returns database connection
 func GetDBConn() *gorm.DB {
@@ -29,7 +55,7 @@ func GetDBConfig() string {
 	}
 
 	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
+	password := getDatabasePassword()
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	database_name := os.Getenv("DB_DATABASE_NAME")
