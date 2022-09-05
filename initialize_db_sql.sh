691adc7abbf6ce329fc37ba0941c9f3151ca4dbc
keisuke <mrtambourineman8358@gmail.com>
2022-09-05 23:02:08 +0900

    make sure can use aws cli

---
+++ initialize_db_sql.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+$DB_PASSWORD = (`aws ssm get-parameters --names /tranaza/DB_PASSWORD --query "Parameters[*].{Value: Value}"  --output text`)
+
+echo $DB_PASSWORD
+
+mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p$DB_PASSWORD trout_analyzer < "./db/mysql_init/00_create_tables.sql"
