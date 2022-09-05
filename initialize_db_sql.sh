b3d2b38f8d4e13acdaacd4e4450d45c93d748da7
keisuke <mrtambourineman8358@gmail.com>
2022-09-05 23:31:24 +0900

    tried again

--- initialize_db_sql.sh
+++ initialize_db_sql.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 DB_PASSWORD=(`aws ssm get-parameters --names "/tranaza/DB_PASSWORD" --query "Parameters[*].{Value: Value}" --with-decryption --output text`)
 
-echo $DB_PASSWORD
-
 mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p${DB_PASSWORD}
