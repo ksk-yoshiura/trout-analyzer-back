7c0bf446f6ec0a5c63896552a263ac98bcf190f8
keisuke <mrtambourineman8358@gmail.com>
2022-09-20 22:39:27 +0900

    unnecessary

--- initialize_db_sql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DB_PASSWORD=(`aws ssm get-parameters --names "/tranaza/DB_PASSWORD" --query "Parameters[*].{Value: Value}" --with-decryption --output text`)
-echo $DB_PASSWORD
-mysql -h tranaza-prod-tranaza.csbzfseof6ef.ap-northeast-1.rds.amazonaws.com --port 3306 -u tranaza -p${DB_PASSWORD}
-# aws ecs execute-command --cluster tranaza-prod-tranaza --task f8263624a62d49dbabd4d6a97744fbb0 --container golang --interactive --command 
